f8942896e5e605b51c15b47d8d5c0bc9b8dd96a7
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-10-04 17:11:57 +0530

    portfolio added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { Container } from "./common"
+
+import { portfolios } from "../data"
 
 import "./portfolio.css"
 
 const Portfolio = () => {
+  const portfoliosName = Object.keys(portfolios)
+  const [selectedPortfolio, setSelectedPortfolio] = useState(portfoliosName[0])
+  const selectedFeaturedPortfolios = portfolios[selectedPortfolio].filter(
+    portfolio => portfolio.type === "featured"
+  )
+  const selectedPortfolios = portfolios[selectedPortfolio].filter(
+    portfolio => portfolio.type === "regular"
+  )
   return (
     <div className="portfolio-area">
       <Container>
@@ -13,57 +23,56 @@ const Portfolio = () => {
         </div>
         <div className="portfolios">
           <ul className="portfolio-nav">
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
+            {portfoliosName.map(name => (
+              <li
+                onClick={() => setSelectedPortfolio(name)}
+                className={name === selectedPortfolio ? "active" : ""}
+                key={name}
+              >
+                {name}
+              </li>
+            ))}
           </ul>
-          <div className="portfolio">
-            <div>Image</div>
-            <h4>Title</h4>
-            <p>Little bit description</p>
-            <ul className="portfolio-action">
-              <li>
-                <a href="#">Link1</a>
-              </li>
-              <li>
-                <a href="#">Link1</a>
-              </li>
-              <li>
-                <a href="#">Link1</a>
-              </li>
-            </ul>
+          <div className="portfolio-items portfolios-featured">
+            {selectedFeaturedPortfolios.map(portfolio => (
+              <div key={portfolio.name} className="portfolio">
+                <div className="portfolio-img">
+                  <img alt={portfolio.name} src={portfolio.image} />
+                </div>
+                <div className="details">
+                  <h4>
+                    <a href={portfolio.demo}>{portfolio.name}</a>
+                  </h4>
+                  <p>{portfolio.description}</p>
+                  <ul className="portfolio-tools">
+                    {portfolio.tools.map(tool => (
+                      <li key={tool}>
+                        <a href="#">{tool}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="portfolio">
-            <div>Image</div>
-            <h4>Title</h4>
-            <p>Little bit description</p>
-            <ul className="portfolio-action">
-              <li>
-                <a href="#">Link1</a>
-              </li>
-              <li>
-                <a href="#">Link1</a>
-              </li>
-              <li>
-                <a href="#">Link1</a>
-              </li>
-            </ul>
-          </div>
-          <div className="portfolio">
-            <div>Image</div>
-            <h4>Title</h4>
-            <p>Little bit description</p>
-            <ul className="portfolio-action">
-              <li>
-                <a href="#">Link1</a>
-              </li>
-              <li>
-                <a href="#">Link1</a>
-              </li>
-              <li>
-                <a href="#">Link1</a>
-              </li>
-            </ul>
+          <div className="portfolio-items portfolios-regular">
+            {selectedPortfolios.map(portfolio => (
+              <div key={portfolio.name} className="portfolio">
+                <div className="details">
+                  <h4>
+                    <a href={portfolio.demo}>{portfolio.name}</a>
+                  </h4>
+                  <p>{portfolio.description}</p>
+                  <ul className="portfolio-tools">
+                    {portfolio.tools.map(tool => (
+                      <li key={tool}>
+                        <a href="#">{tool}</a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </Container>
---
+++ src/data/portfolios.js
@@ -0,0 +1,197 @@
+export const portfolios = {
+  Web: [
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+  ],
+  Mobile: [
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+  ],
+  Others: [
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "featured",
+      name: "Something",
+      image:
+        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+    {
+      type: "regular",
+      name: "Something",
+      image: "somewhere",
+      description:
+        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
+      demo: "",
+      source: "",
+      tools: ["nodejs", "expressjs", "graphql"],
+    },
+  ],
+}

998d27fc264b9f83b22b210664db453284a05b14
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-10-07 17:08:55 +0530

    contact area done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Container, Title } from "./common"
+import { Container, Title, Button } from "./common"
 
 import "./contact.css"
 
@@ -10,35 +10,56 @@ const Contact = () => {
       <Container>
         <Title side="right" title="Contact" />
         <div className="contact">
+          <div className="contact-status">
+            <p>
+              Although I am currently studying for my bachelor's degree I will
+              be free from January 2020. And I am interested in working with any
+              company from January 2020 that thinks my skill will be helpful for
+              them. If you are looking for someone like me, please let me know.
+              Or you can just 'say hi' to me.
+            </p>
+            <div>
+              <Button
+                title="Contact Me"
+                mt="25px"
+                bgColor="#00cf5d"
+                color="#fff"
+                link="mailto:zonayedpca@gmail.com"
+              />
+            </div>
+          </div>
           <div className="contact-details">
             <ul>
               <li>
                 <h5>Email</h5>
-                <p>email@email.com</p>
+                <p>
+                  zonayedpca@gmail.com <span>(Recommended)</span>
+                </p>
               </li>
               <li>
                 <h5>Skype</h5>
-                <p>zonayedpca</p>
+                <p>
+                  zonayedpca <span>(Always Available)</span>
+                </p>
               </li>
               <li>
                 <h5>Phone</h5>
-                <p>+91-7347075818</p>
+                <p>
+                  +8801814274346 <span>(Bangladesh)</span> / +917347075818{" "}
+                  <span>(India)</span>
+                </p>
+              </li>
+              <li>
+                <h5>Social</h5>
+                <p>
+                  Facebook/Twitter - @zonayedpca <span>(Slow response)</span>
+                </p>
               </li>
               <li>
                 <h5>Address</h5>
-                <p>Cumilla, Bangladesh</p>
+                <p>Gouripur, Daudkandi, Cumilla, Bangladesh - 3517</p>
               </li>
             </ul>
-          </div>
-          <div className="contact-form">
-            <form>
-              <input name="name" placeholder="Your name" />
-              <input name="email" placeholder="Your email" />
-              <textarea name="message" placeholder="Your message"></textarea>
-              <button type="submit" className="btn">
-                Submit
-              </button>
-            </form>
           </div>
         </div>
       </Container>

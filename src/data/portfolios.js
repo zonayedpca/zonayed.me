a42a46bf2d0bdcbb07708018a7a0a0daf221380f
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-10-13 18:44:50 +0530

    web projects added

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -2,131 +2,96 @@ export const portfolios = {
   Web: [
     {
       type: "featured",
-      name: "Something",
+      name: "DevSonket",
       image:
-        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
+        "https://repository-images.githubusercontent.com/146095243/8ddf6680-ede6-11e9-93a2-508721b67761",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://devsonket.com",
+      source: "https://github.com/devsonket/devsonket.github.io",
+      tools: ["gatsby", "json", "travis ci"],
     },
     {
       type: "featured",
-      name: "Something",
+      name: "js.zonayed.me",
       image:
         "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://zonayed.js.org",
+      source: "https://github.com/zonayedpca/js.zonayed.me",
+      tools: ["gatsby", "utterances", "travis ci"],
     },
     {
       type: "featured",
-      name: "Something",
+      name: "react.zonayed.me",
       image:
         "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://react.zonayed.me",
+      source: "https://github.com/zonayedpca/react.zonayed.me",
+      tools: ["react", "wp-rest", "travis ci"],
     },
     {
       type: "regular",
-      name: "Something",
+      name: "Phonebook",
       image: "somewhere",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://phonebookpca.netlify.com/",
+      source: "https://github.com/zonayedpca/nodeMySQLPhonebook",
+      tools: ["express", "mysql", "react"],
     },
     {
       type: "regular",
-      name: "Something",
+      name: "Holy Quran",
       image: "somewhere",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://zonayedpca.github.io/HolyQuranReact/",
+      source: "https://github.com/zonayedpca/HolyQuranReact",
+      tools: ["react", "redux"],
     },
     {
       type: "regular",
-      name: "Something",
+      name: "To-do App",
       image: "somewhere",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
-    },
-  ],
-  Mobile: [
-    {
-      type: "featured",
-      name: "Something",
-      image:
-        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
-      description:
-        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
-    },
-    {
-      type: "featured",
-      name: "Something",
-      image:
-        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
-      description:
-        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
-    },
-    {
-      type: "featured",
-      name: "Something",
-      image:
-        "https://repository-images.githubusercontent.com/207064451/642d9680-dca3-11e9-8494-69ab89ac8f31",
-      description:
-        "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://todoreacthooks.netlify.com/",
+      source: "https://github.com/zonayedpca/todo-react-hooks",
+      tools: ["react", "react hooks"],
     },
     {
       type: "regular",
-      name: "Something",
+      name: "Micro Blog",
       image: "somewhere",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://zonayedpca.github.io/react-micro-blog/",
+      source: "https://github.com/zonayedpca/react-micro-blog",
+      tools: ["react", "react router"],
     },
     {
       type: "regular",
-      name: "Something",
+      name: "Mini Blog",
       image: "somewhere",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://zonayedpca.github.io/react-mini-blog/",
+      source: "https://github.com/zonayedpca/react-mini-blog",
+      tools: ["react", "react router", "draft js"],
     },
     {
       type: "regular",
-      name: "Something",
+      name: "PWA Blog",
       image: "somewhere",
       description:
         "A Liitle bit decrip from soemhw someday so that you ca get it little bit anyway",
-      demo: "https://google.com",
-      source: "https://google.com",
-      tools: ["nodejs", "expressjs", "graphql"],
+      demo: "https://zonayedjs.netlify.com/",
+      source: "https://github.com/zonayedpca/old.js.zonayed.me",
+      tools: ["react", "pwa"],
     },
   ],
   Others: [
